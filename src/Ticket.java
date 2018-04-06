5f94baf45a71b196c62017e847b84eded95c9471
Anna812 <88menyus@gmail.com>
2018-04-06 19:31:11 +0200

    create validation for reporting bug only in working hours

--- src/Ticket.java
+++ src/Ticket.java
@@ -5,9 +5,19 @@ public class Ticket {
     public int turnaroundTime;
     public Date dueDate;
 
-    public Ticket(int turnaroundTime) {
-        timeOfReport = new Date();
-        this.turnaroundTime = turnaroundTime;
+    public Ticket(int turnaroundTime) throws Exception {
+        if(checkIfWorktime()) {
+            timeOfReport = new Date();
+            this.turnaroundTime = turnaroundTime;
+        } else {
+            throw new Exception("You cannot report new bug outside working hours");
+        }
+    }
+
+    private boolean checkIfWorktime() {
+        Date now = new Date();
+        int nowHour = now.getHours();
+        return (nowHour < 17 && nowHour > 9);
     }
 
     public void calculateDueDate() {
