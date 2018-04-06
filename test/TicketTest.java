b2bf5d41f5553b4e18b54def6c2beb5f5a4f4e16
Anna812 <88menyus@gmail.com>
2018-04-06 23:06:47 +0200

    create test method for new month and new year

--- test/TicketTest.java
+++ test/TicketTest.java
@@ -10,7 +10,7 @@ public class TicketTest {
     @Test
     public void checkTicketConstructorValidationTestOutofWorkingHours(){
         try {
-            Ticket ticket = new Ticket(1L);
+            new Ticket(1L);
             fail();
         } catch (Exception e) {
             assertEquals("You cannot report new bug outside working hours", e.getMessage());
@@ -20,7 +20,7 @@ public class TicketTest {
     @Test
     public void checkTicketConstructorValidationTestWeekend(){
         try {
-            Ticket ticket = new Ticket(1L);
+            new Ticket(1L);
             fail("exception is expected if test runs on weekend");
         } catch (Exception e) {
             assertEquals("You cannot report new bug outside working hours", e.getMessage());
@@ -29,49 +29,57 @@ public class TicketTest {
 
     @Test
     public void calculateDueDateTestTurnaroundIsOneHour() {
-        Ticket ticket = new Ticket(1, LocalDateTime.of(2018, Month.APRIL, 06, 9, 00));
+        Ticket ticket = new Ticket(1, LocalDateTime.of(2018, Month.APRIL, 6, 9, 0));
         ticket.calculateDueDate();
-        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 06, 10, 00);
+        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 6, 10, 0);
         assertEquals(result, ticket.dueDate);
     }
 
     @Test
     public void calculateDueDateTestTurnaroundIsEightHours() {
-        Ticket ticket = new Ticket(8, LocalDateTime.of(2018, Month.APRIL, 04, 9, 00));
+        Ticket ticket = new Ticket(8, LocalDateTime.of(2018, Month.APRIL, 4, 9, 0));
         ticket.calculateDueDate();
-        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 05, 9, 00);
+        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 5, 9, 0);
         assertEquals(result, ticket.dueDate);
     }
 
     @Test
     public void calculateDueDateTestTurnaroundIsEightHoursMidDay() {
-        Ticket ticket = new Ticket(8, LocalDateTime.of(2018, Month.APRIL, 04, 10, 00));
+        Ticket ticket = new Ticket(8, LocalDateTime.of(2018, Month.APRIL, 4, 10, 0));
         ticket.calculateDueDate();
-        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 05, 10, 00);
+        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 5, 10, 0);
         assertEquals(result, ticket.dueDate);
     }
 
     @Test
     public void calculateDueDateTestTurnaroundIsNineHours() {
-        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 04, 9, 00));
+        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 4, 9, 0));
         ticket.calculateDueDate();
-        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 05, 10, 00);
+        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 5, 10, 0);
         assertEquals(result, ticket.dueDate);
     }
 
     @Test
     public void calculateDueDateTestTurnaroundIsNineHoursMidDay() {
-        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 04, 10, 00));
+        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 4, 10, 0));
         ticket.calculateDueDate();
-        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 05, 11, 00);
+        LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 5, 11, 0);
         assertEquals(result, ticket.dueDate);
     }
 
     @Test
     public void calculateDueDateTestTurnaroundIsNineHoursSaturday() {
-        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 06, 10, 00));
+        Ticket ticket = new Ticket(9, LocalDateTime.of(2018, Month.APRIL, 6, 10, 00));
         ticket.calculateDueDate();
         LocalDateTime result = LocalDateTime.of(2018, Month.APRIL, 9, 11, 00);
+        assertEquals(result, ticket.dueDate);
+    }
+
+    @Test
+    public void calculateDueDateTestNewMonth() {
+        Ticket ticket = new Ticket(9, LocalDateTime.of(2017, Month.DECEMBER, 29, 10, 00));
+        ticket.calculateDueDate();
+        LocalDateTime result = LocalDateTime.of(2018, Month.JANUARY, 1, 11, 00);
         assertEquals(result, ticket.dueDate);
     }
 }
