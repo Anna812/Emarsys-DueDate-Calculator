428689b7db3c7fe21baf0f33eda6db784c8c3bf5
Anna812 <88menyus@gmail.com>
2018-04-06 17:52:03 +0200

    add constructor to Ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -4,4 +4,9 @@ public class Ticket {
     public Date timeOfReport;
     public int turnaroundTime;
     public Date dueDate;
+
+    public Ticket(int turnaroundTime) {
+        timeOfReport = new Date();
+        this.turnaroundTime = turnaroundTime;
+    }
 }
