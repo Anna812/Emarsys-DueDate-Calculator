9f4ce12ef8e73be7fd7a2b4b5d9ee76f10b5b9da
Anna812 <88menyus@gmail.com>
2018-04-06 19:19:48 +0200

    write test method for case turnover time is one hour

--- test/TicketTest.java
+++ test/TicketTest.java
@@ -1,15 +1,26 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class TicketTest {
 
+    Ticket ticketA;
+
     @Before
     public void setUp() throws Exception {
+       ticketA = new Ticket(1);
     }
 
     @Test
-    public void calculateDueDate() {
+    public void calculateDueDateTestTurnaroundIsOneHour() {
+        Date result = new Date(2018, Calendar.APRIL, 06, 10, 00);
+        ticketA.timeOfReport = new Date(2018, Calendar.APRIL, 06, 9, 00);
+        ticketA.calculateDueDate();
+        assertEquals(result, ticketA.dueDate);
     }
 }
